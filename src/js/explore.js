a10bd5ae2df5b7ae5d979d20867bd9fba83718a6
synokira <contact@synokira.com>
2024-05-20 02:27:20 +0300

    chore: Update logo loading attribute in header and footer layouts

--- src/js/explore.js
+++ src/js/explore.js
@@ -1,35 +1,31 @@
-document.addEventListener("DOMContentLoaded", () => {
-	const tabs = document.querySelectorAll(".tab");
+const tabs = document.querySelectorAll('.tab')
 
-	tabs.forEach((tab, index) => {
-		if (index !== 0) {
-			tab.addEventListener("mouseover", () => {
-				tabs[0].style.width = "5%";
-			});
-		}
-	});
-
-	document
-		.querySelector(".images-accordion")
-		.addEventListener("mouseout", () => {
-			tabs[0].style.width = "100%";
-		});
-
-	const links = document.querySelectorAll(".explore-nav a");
-
-	for (const link of links) {
-		link.addEventListener("click", (event) => {
-			event.preventDefault();
-
-			const containers = document.querySelectorAll(".imageContainer");
-			for (const container of containers) {
-				container.style.display = "none";
-			}
-
-			const target = link.getAttribute("href").slice(1);
-
-			const container = document.querySelector(`#${target}`);
-			container.style.display = "grid";
-		});
+tabs.forEach((tab, index) => {
+	if (index !== 0) {
+		tab.addEventListener('mouseover', () => {
+			tabs[0].style.width = '5%'
+		})
 	}
-});
+})
+
+document.querySelector('.images-accordion').addEventListener('mouseout', () => {
+	tabs[0].style.width = '100%'
+})
+
+const links = document.querySelectorAll('.explore-nav a')
+
+for (const link of links) {
+	link.addEventListener('click', (event) => {
+		event.preventDefault()
+
+		const containers = document.querySelectorAll('.imageContainer')
+		for (const container of containers) {
+			container.style.display = 'none'
+		}
+
+		const target = link.getAttribute('href').slice(1)
+
+		const container = document.querySelector(`#${target}`)
+		container.style.display = 'grid'
+	})
+}
--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,41 @@
 //Sticky Header
-$(window).on("scroll", () => {
-	const header = $("header");
-	header.toggleClass("sticky", $(window).scrollTop() > 0);
-});
+$(window).on('scroll', () => {
+	const header = $('header')
+	header.toggleClass('sticky', $(window).scrollTop() > 0)
+})
+$('DOMContentLoaded', () => {
+	const iconMenuMobile = $('.icon-menu-mobile')
+	const menuMobile = $('.menu-mobile')
+	const iconClose = $('.menu-icon-close')
 
-// Mobile Menu
-const iconMenuMobile = $(".icon-menu-mobile");
-const menuMobile = $(".menu-mobile");
-const menuIconClose = $(".menu-icon-close");
-const menuMobileNav = $(".menu-mobile-nav");
+	iconMenuMobile.on('click', () => {
+		menuMobile.addClass('active')
+	})
 
-iconMenuMobile.on("click", () => {
-	menuMobile.toggleClass("active");
-});
-
-menuIconClose.on("click", () => {
-	menuMobile.removeClass("active");
-});
-
-menuMobileNav.on("click", () => {
-	menuMobile.removeClass("active");
-});
+	iconClose.on('click', () => {
+		menuMobile.removeClass('active')
+	})
+})
 
 // Accordion
-const acc = $(".accordion-header");
+$('.accordion-header').on('click', (event) => {
+	const clickedHeader = $(event.currentTarget)
 
-acc.on("click", (event) => {
-	const clickedHeader = $(event.currentTarget);
+	const activeHeaders = $('.accordion-header.active')
 
-	const activeHeaders = $(".accordion-header.active");
-
-	activeHeaders.each((index, activeHeader) => {
+	activeHeaders.each((_, activeHeader) => {
 		if (activeHeader !== clickedHeader[0]) {
-			$(activeHeader).removeClass("active");
-			$(activeHeader).next().css("maxHeight", "");
+			$(activeHeader).removeClass('active')
+			$(activeHeader).next().css('maxHeight', '')
 		}
-	});
+	})
 
-	if (clickedHeader.hasClass("active")) {
-		clickedHeader.removeClass("active");
-		clickedHeader.next().css("maxHeight", "");
+	if (clickedHeader.hasClass('active')) {
+		clickedHeader.removeClass('active')
+		clickedHeader.next().css('maxHeight', '')
 	} else {
-		clickedHeader.addClass("active");
-		const panel = clickedHeader.next();
-		panel.css("maxHeight", `${panel[0].scrollHeight}px`);
+		clickedHeader.addClass('active')
+		const panel = clickedHeader.next()
+		panel.css('maxHeight', `${panel[0].scrollHeight}px`)
 	}
-});
+})
